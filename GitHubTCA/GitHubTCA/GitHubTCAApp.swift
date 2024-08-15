02b05e22ff1a46c318312bb81b9caa81ea378f48
andre <andresutanto2007@gmail.com>
2024-08-15 11:31:26 +0900

    add githubtca project

---
+++ GitHubTCA/GitHubTCA/GitHubTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTCAApp.swift
+//  GitHubTCA
+//
+//  Created by Andre on 2024/08/15.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
